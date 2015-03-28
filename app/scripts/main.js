58c25cb55417f5e238e5ff9957a3acb105c02be7
agrande <alberto.grande@gmail.com>
2015-03-28 11:29:41 +0100

    First code

---
+++ app/scripts/main.js
@@ -0,0 +1,42 @@
+
+function activateWithID(id) {
+
+	var pswpElement = document.querySelectorAll('.pswp')[0];
+
+	// build items array
+	var items = [
+	    {
+	        src: 'http://placehold.it/600x400',
+	        w: 600,
+	        h: 400,
+	        msrc: 'http://placehold.it/60x40'
+	    },
+	    {
+	        src: 'http://placehold.it/1200x900',
+	        w: 1200,
+	        h: 900,
+	        msrc: 'http://placehold.it/120x90'
+	    },
+	    {
+	        src: 'http://placehold.it/800x600',
+	        w: 800,
+	        h: 600,
+	        msrc: 'http://placehold.it/800x600'
+	    }
+	];
+
+	// define options (if needed)
+	var options = {
+	    // optionName: 'option value'
+	    // for example:
+	    index: id // start at first slide
+	};
+
+	// Initializes and opens PhotoSwipe
+	var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+	gallery.init();
+}
+
+function activate() {
+	activateWithID(0);
+}
